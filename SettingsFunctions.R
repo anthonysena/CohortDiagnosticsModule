dfaa2f5c051139a9368574a7ec59ee7942e083f0
Anthony Sena <asena5@its.jnj.com>
2022-06-29 12:26:32 -0400

    v0.0.8

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -17,7 +17,7 @@ createCohortDiagnosticsModuleSpecifications <- function(runInclusionStatistics =
   }
 
   specifications <- list(module = "CohortDiagnosticsModule",
-                         version = "0.0.7",
+                         version = "0.0.8",
                          remoteRepo = "github.com",
                          remoteUsername = "anthonysena",
                          settings = analysis)
